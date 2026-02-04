2ffc0b13f3ccbe0eabc2746743434d221744e0a1
Eivind <eivindh3@gmail.com>
2026-02-04 11:52:10 +0100

    added base files

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from 'node:path'
+import tailwind from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	server: {
+		port: 1337
+	},
+	plugins: [react(), tailwind()],
+	resolve: {
+		alias: {
+			'~': path.resolve(__dirname, './src')
+		}
+	}
+})
