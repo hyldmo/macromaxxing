3d07e33f8f8bca68dfde39683f1af91b34a4d503
Eivind <eivindh3@gmail.com>
2026-02-07 11:55:47 +0100

    build: add source map

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
-		outDir: 'workers/dist'
+		outDir: 'workers/dist',
+		sourcemap: true
 	},
 	server: {
 		port: 1337,
